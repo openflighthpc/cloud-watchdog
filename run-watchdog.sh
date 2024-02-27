4bd20de62e43f5f033abacec93bd6da71370705d
Stu Franks <stu@stuts.uk>
2024-02-27 16:13:15 +0000

    Add some logging

--- run-watchdog.sh
+++ run-watchdog.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # Get directory of script for locating templates and config
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -6,6 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/settings.sh
 
 OUT=$(bash $DIR/watchdog.sh)
+
+echo "$(date) - Cloud Watchdog Output Logging" >> /var/log/cloud-watchdog.log
+echo "$OUT" >> /var/log/cloud-watchdog.log
+echo '' >> /var/log/cloud-watchdog.log
 
 if [[ "$(echo "$OUT" |wc -l)" -eq "3" ]] ; then
     echo "No running systems to shut down"
