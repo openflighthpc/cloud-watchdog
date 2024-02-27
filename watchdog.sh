14de53ab41ad19182526ac002fa7e2a4e9636bb1
Stu Franks <stu@stuts.uk>
2024-02-27 16:13:22 +0000

    Explicit path to aws binary

--- watchdog.sh
+++ watchdog.sh
@@ -1,4 +1,4 @@
-#!/bin.bash
+#!/bin/bash -l
 
 # Mention @stupot & @devs
 echo "# Cloud Watchdog - <@U4ZSTKBPA> <!subteam^S0DL8PF0A>\n\n"
@@ -8,17 +8,17 @@ echo "# Cloud Watchdog - <@U4ZSTKBPA> <!subteam^S0DL8PF0A>\n\n"
 #
 echo "## AWS\n"
 
-AWS_REGIONS=$(aws ec2 describe-regions --no-all-regions --query 'Regions[*].RegionName' --output text)
+AWS_REGIONS=$(/usr/local/bin/aws ec2 describe-regions --no-all-regions --query 'Regions[*].RegionName' --output text)
 
 # Loop through regions 
 
 for region in $AWS_REGIONS ; do 
     ## Identify running instances *without* tag
-    INSTANCES=$(aws --region $region ec2 describe-instances --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[?!not_null(Tags[?Key == `WatchdogIgnore`])] | [].InstanceId' --output text)
+    INSTANCES=$(/usr/local/bin/aws --region $region ec2 describe-instances --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[?!not_null(Tags[?Key == `WatchdogIgnore`])] | [].InstanceId' --output text)
   
     ## Shutdown Instances
     for instance in $INSTANCES ; do
-        info=$(aws --region $region ec2 describe-instances --instance-id $instance --output yaml)
+        info=$(/usr/local/bin/aws --region $region ec2 describe-instances --instance-id $instance --output yaml)
         NAME=$(echo "$info" |grep 'Key: Name' -A 1 |grep Value |sed 's/.*: //g')
 
         if [[ -z $NAME ]] ; then
@@ -26,15 +26,15 @@ for region in $AWS_REGIONS ; do
         fi
 
         ## Identify user from RunInstances cloudtrail log message
-        USER=$(aws --region $region cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceName,AttributeValue=$instance --query 'Events[*].{event:EventName,user:Username}' --output yaml |grep RunInstances -A 1 |grep user |sed 's/.*: //g')
+        USER=$(/usr/local/bin/aws --region $region cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceName,AttributeValue=$instance --query 'Events[*].{event:EventName,user:Username}' --output yaml |grep RunInstances -A 1 |grep user |sed 's/.*: //g')
 
         ## If user empty then figure it out from whatever users recently performed actions on resources
         if [[ -z $USER ]] ; then 
-            USER=$(aws --region $region cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceName,AttributeValue=$instance |grep Username |sort |uniq |sed 's/.*: //g' |tr '\n' ' ')
+            USER=$(/usr/local/bin/aws --region $region cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceName,AttributeValue=$instance |grep Username |sort |uniq |sed 's/.*: //g' |tr '\n' ' ')
         fi
 
         echo "Shutting down ${USER}'s instance in $region ($instance - $NAME)\n" 
-        stop=$(aws --region $region ec2 stop-instances --instance-ids $instance)
+        stop=$(/usr/local/bin/aws --region $region ec2 stop-instances --instance-ids $instance)
     done 
 done
 
